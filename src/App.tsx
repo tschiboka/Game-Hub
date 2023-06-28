e8990a56b8342bfd04c726decda4a60df7e299d3
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-06-28 22:48:27 +0100

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
 function App() {
-    return <></>;
+    return (
+        <div>
+            <Button colorScheme="blue">Button</Button>
+        </div>
+    );
 }
 
 export default App;
