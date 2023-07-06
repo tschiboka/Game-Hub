e86234a6f5ca3c4596a4cb4c85675861fccf7776
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-06 05:06:11 +0100

    Refactor Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,14 +13,15 @@ interface Props {
 export default function GameGrid({ gameQuery }: Props) {
     const { data, error, isLoading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6];
+    if (error)
+        return (
+            <Text align="center">
+                Couldn't Fetch Data <br />
+                {error}
+            </Text>
+        );
     return (
         <>
-            {error && (
-                <Text align="center">
-                    Couldn't Fetch Data <br />
-                    {error}
-                </Text>
-            )}
             <GameHeading gameQuery={gameQuery} />
             <SimpleGrid
                 columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
