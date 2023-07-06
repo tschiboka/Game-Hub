ed1eaf9df22e7f80cc38e9cacced5c799f62844c
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-06 03:51:34 +0100

    Handle Games with No Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeHolderImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 export default function getCroppedImageURL(url: string) {
-    if (!url) return "";
+    if (!url) return placeHolderImage;
     const delimiter = "media/";
     const index = url.indexOf(delimiter) + delimiter.length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
